2089f6ef788f8b33e6a0350070a050773f31b3f9
marcio-vitor <marciovitor.fla33@gmail.com>
2025-08-14 10:38:40 -0300

    Add files via upload
    nota do bimestres aula 14/08

---
+++ nota do bimestre.py
@@ -0,0 +1,4 @@
+n1=float(input("digite sua nota do 1° bimestre:"))
+n2=float(input("digite sua nota do 2° bimestre:"))
+n3=((60-(n1*2+n2*2))/2)/3
+print("você precisa tirar",n3, "voce precisa tirar nos proximo 2 bimestres")
