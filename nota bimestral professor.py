ac2ecb55484dbf1e9179f27f23bb46075b7f0489
marcio-vitor <marciovitor.fla33@gmail.com>
2025-08-19 08:07:54 -0300

    nota bimestral que o professor fez correção

---
+++ nota bimestral professor.py
@@ -0,0 +1,4 @@
+nota1=float(input("digite a primeira nota:"))
+nota2=float(input("digite a segunda nota:"))
+notafalta=60-(nota1*2+nota2*2)
+print("você precisa tira",((notafalta/2)/3),"nos proximos 2 bimestres!")
