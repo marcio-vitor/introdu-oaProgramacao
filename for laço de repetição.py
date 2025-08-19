29be8bdadb29c32855deb87d5d43b184a5b5643a
marcio-vitor <marciovitor.fla33@gmail.com>
2025-08-19 08:45:10 -0300

    laço de repetiçao  tabuada de 1 a 10

---
+++ for laço de repetição.py
@@ -0,0 +1,5 @@
+print("esta aplicação mostra a tabuada de 1 a 10 de um numero digitado")
+numero=int(input("Digite um valor: "))
+for i in range(10):
+    print(numero,"x",i+1,"=",numero*(i+1))
+    
