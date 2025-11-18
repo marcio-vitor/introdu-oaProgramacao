e09f080e115f66724caa4e00159786084573b882
marcio-vitor <marciovitor.fla33@gmail.com>
2025-11-18 08:04:26 -0300

    Update robo agricola.py

--- robo agricola.py
+++ robo agricola.py
@@ -1,11 +1,10 @@
-
+#robo agricola
 bateria = int(input("Nível de bateria (0 a 100): "))
 temperatura = float(input("Temperatura do ambiente (°C): "))
 umidade = int(input("Umidade do solo (0 a 100): "))
 modo = input("Modo de operação (plantio, colheita ou irrigacao): ")
 
-
-
+#bateria
 if bateria < 20:
     print("Bateria muito baixa! Retorne imediatamente para a base.")
 
@@ -15,22 +14,19 @@ if bateria >= 20:
 
 if bateria >= 50:
     print("Bateria suficiente para operação.")
-
-
+#temperatura
 if temperatura > 40:
     print("Temperatura crítica! Operação suspensa.")
 
 if temperatura < 5:
     print("Frio extremo! Modo de economia ativado.")
-
-
+#umidade
 if umidade < 30:
     print("Solo muito seco. Recomendado iniciar irrigação.")
 
 if umidade > 80:
     print("Solo encharcado! Suspenda irrigação imediatamente.")
-
-
+#modo
 if modo == "plantio":
     print("Iniciando modo PLANTIO...")
 
@@ -40,28 +36,22 @@ if modo == "colheita":
 if modo == "irrigacao":
     print("Iniciando modo IRRIGAÇÃO...")
 
-
 bateria_ok = 0
 temperatura_ok = 0
 umidade_ok = 0
 
-
 if bateria >= 50:
     bateria_ok = 1
-
 
 if temperatura >= 10:
 
     if temperatura <= 35:
         temperatura_ok = 1
 
-
 if umidade >= 30:
 
     if umidade <= 80:
         umidade_ok = 1
-
-
 
 if bateria_ok == 1:
     if temperatura_ok == 1:
